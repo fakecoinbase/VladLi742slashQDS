b0d54e5e4ea6122140ef05de5a989ae6ec1747ed
VladLi742 <vladislav.deev742@gmail.com>
2020-04-03 22:13:48 +0300

    optimize websocket.js

--- src/websocket.js
+++ src/websocket.js
@@ -1,32 +1,22 @@
-const Api = { data: [] };
+const Api = { data: {} };
 
 const createWebSocketConnection = () => {
-    // return new Promise((resolve, reject) => {
-        const apiKey = "6cb0216625d1aaab0ce177410c490178da22c3a32c98dd64a1692760bb2b72e4";
-        const socket = new WebSocket('wss://streamer.cryptocompare.com/v2?api_key=' + apiKey);
+    const apiKey = "6cb0216625d1aaab0ce177410c490178da22c3a32c98dd64a1692760bb2b72e4";
+    const socket = new WebSocket('wss://streamer.cryptocompare.com/v2?api_key=' + apiKey);
 
-        socket.onopen = function () {
-            const subRequest = {
-                "action": "SubAdd",
-                "subs": ["0~Coinbase~BTC~USD", "0~Coinbase~ETH~USD"],
-            };
-            socket.send(JSON.stringify(subRequest));
+    socket.onopen = function () {
+        const subRequest = {
+            "action": "SubAdd",
+            "subs": ["0~Coinbase~BTC~USD", "0~Coinbase~ETH~USD"],
         };
+        socket.send(JSON.stringify(subRequest));
+    };
 
-        socket.onerror = function (err) {
-            // reject(err);
-        };
-
-        socket.onmessage = function onStreamMessage(event) {
-            if (JSON.parse(event.data)["TYPE"] === "0") {
-                Api.data = event.data;
-            }
-        };
-
-        socket.onclose = function (event) {
-            // reject();
+    socket.onmessage = function onStreamMessage(event) {
+        if (JSON.parse(event.data)["TYPE"] === "0") {
+            Api.data = event.data;
         }
-    // })
+    };
 };
 
 createWebSocketConnection();
