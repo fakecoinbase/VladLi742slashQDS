2b55e41a16d334169a541bd3c9dbf9934e560030
VladLi742 <vladislav.deev742@gmail.com>
2020-04-04 15:25:02 +0300

    remove wrappers from src/index.js to src/App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
+import { store, persistor } from './store';
 import Main from "./Main/container";
 
 export default function() {
-    return <Main/>;
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <React.StrictMode>
+                    <Main/>
+                </React.StrictMode>
+            </PersistGate>
+        </Provider>
+    );
 };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import { store, persistor } from './store';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <React.StrictMode>
-                <App/>
-            </React.StrictMode>
-        </PersistGate>
-    </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
