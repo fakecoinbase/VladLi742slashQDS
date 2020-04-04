b32a427d2fbc2a00e7118d8d56d81ade09b873b0
VladLi742 <vladislav.deev742@gmail.com>
2020-04-04 18:45:40 +0300

    make connection in a react-saga channel

--- src/sagas.js
+++ src/sagas.js
@@ -1,25 +1,76 @@
-import { all, put, takeLatest, delay } from 'redux-saga/effects';
+import { eventChannel, END } from 'redux-saga';
+import { put, call, take, takeLatest, all } from 'redux-saga/effects';
+import { FETCH_DATA, FETCH_DATA_FAILED, FETCH_DATA_SUCCEEDED } from "./components/Table/actions";
 
-import Api from './websocket.js';
+function createEventChannel(ws) {
+    return eventChannel(emit => {
+        function createWs() {
+            ws.onopen = () => {
+                const subRequest = {
+                    "action": "SubAdd",
+                    "subs": ["0~Coinbase~BTC~USD", "0~Coinbase~ETH~USD"],
+                };
+                ws.send(JSON.stringify(subRequest));
+            };
 
-function* fetchCoins() {
-    yield delay(1000);
-    try {
-        const data = yield JSON.parse(Api.data);
-        const newData = { text: data.FSYM, value: data.P };
-        yield put({ type: "FETCH_DATA_SUCCEEDED", data: newData });
-        yield put({ type: "FETCH_DATA" });
-    } catch (e) {
-        yield put({ type: "FETCH_DATA_FAILED", message: e.message });
+            ws.onerror = error => {
+                console.log("ERROR: ", error);
+            };
+
+            ws.onmessage = e => {
+                if (JSON.parse(e.data)["TYPE"] === "0") {
+                    return emit(JSON.parse(e.data))
+                }
+            };
+
+            ws.onclose = e => {
+                if (e.code === 1005) {
+                    console.log("WebSocket: closed");
+                    const subRequest = {
+                        "action": "SubRemove",
+                        "subs": ["0~Coinbase~BTC~USD", "0~Coinbase~ETH~USD"],
+                    };
+                    ws.send(JSON.stringify(subRequest));
+                    emit(END);
+                } else {
+                    console.log('Socket is closed Unexpectedly. Reconnect will be attempted in 4 second.', e.reason);
+                    setTimeout(() =>  {
+                        createWs();
+                    }, 4000);
+                }
+            };
+        }
+        createWs();
+
+        return () => {
+            console.log("Closing Websocket");
+            ws.close();
+        };
+    });
+}
+
+function* fetchDataChannel() {
+    const apiKey = "6cb0216625d1aaab0ce177410c490178da22c3a32c98dd64a1692760bb2b72e4";
+    const socket = new WebSocket('wss://streamer.cryptocompare.com/v2?api_key=' + apiKey);
+    const channel = yield call(createEventChannel, socket);
+    while (true) {
+        try {
+            const trade = yield take(channel);
+            const data = { text: trade.FSYM, value: trade.P };
+            yield put({ type: FETCH_DATA_SUCCEEDED, data });
+        }
+        catch(err) {
+            yield put({ type: FETCH_DATA_FAILED, message: err.message });
+        }
     }
 }
 
-function* watchIncrementAsync() {
-    yield takeLatest('FETCH_DATA', fetchCoins);
+function* watchFetchDataAsync() {
+    yield takeLatest(FETCH_DATA, fetchDataChannel);
 }
 
 export default function* rootSaga() {
     yield all([
-        watchIncrementAsync(),
+        watchFetchDataAsync(),
     ])
 }
