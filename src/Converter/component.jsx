cbfbba8c36e094f9bccb3e7f241411a1b1a37781
VladLi742 <vladislav.deev742@gmail.com>
2020-04-05 22:32:29 +0300

    add visual representation to inputs's .onFocus of Converter

--- src/Converter/component.jsx
+++ src/Converter/component.jsx
@@ -1,28 +1,40 @@
 import React from 'react';
-import { InputLabel, TextField } from '@material-ui/core';
+import {
+    InputLabel,
+    TextField,
+    Button,
+} from '@material-ui/core';
 import ImportExportIcon from '@material-ui/icons/ImportExport';
-import Button from '@material-ui/core/Button';
 
-const container = { marginBottom: '30px' };
-const buttonContainer = { margin: '20px 0' };
+const styles = {
+    container: { marginBottom: '30px' },
+    buttonContainer: { margin: '20px 0' }
+};
+
+function setInput(item, ...rest) {
+    const [ rate, changeCount ] = rest;
+    const styleOnFocus = item.onFocus ? { backgroundColor: 'rgb(230, 238, 255, 0.5)' } : {};
+    return (
+        <div>
+            <InputLabel>{item.text}</InputLabel>
+            <TextField
+                value={item.value}
+                onChange={(e) => changeCount(e.target.value, item.text, rate)}
+                fullWidth={true}
+                style={styleOnFocus}
+            />
+        </div>
+    );
+}
 
 export default (props) => {
-    const {
-        fromCurrency,
-        inCurrency,
-        rate,
-        changeCount,
-        invertCurrencies,
-    } = props;
+    const { fromCurrency, inCurrency, invertCurrencies, rate, changeCount } = props;
+    const fromInput = setInput(fromCurrency, rate, changeCount);
+    const toInput = setInput(inCurrency, rate, changeCount);
     return (
-        <div style={container}>
-            <InputLabel>{fromCurrency.text}</InputLabel>
-            <TextField
-                value={fromCurrency.value}
-                onChange={(e) => changeCount(e.target.value, fromCurrency.text, rate)}
-                fullWidth={true}
-            />
-            <div style={buttonContainer}>
+        <div style={styles.container}>
+            {fromInput}
+            <div style={styles.buttonContainer}>
                 <Button
                     onClick={() => invertCurrencies()}
                     fullWidth={true}
@@ -32,12 +44,7 @@ export default (props) => {
                     Поменять местами<ImportExportIcon/>
                 </Button>
             </div>
-            <InputLabel>{inCurrency.text}</InputLabel>
-            <TextField
-                value={inCurrency.value}
-                onChange={(e) => changeCount(e.target.value, inCurrency.text, rate)}
-                fullWidth={true}
-            />
+            {toInput}
         </div>
     );
 };
